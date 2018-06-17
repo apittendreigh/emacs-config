1e0ae2a870db39bf3e2971d755e4b2554c72180f
Alex Pittendreigh <Alex.Pittendreigh@gmail.com>
2018-06-17 15:50:51 +0800

    Added in IDO functionality

--- config.el
+++ config.el
@@ -1,3 +1,8 @@
+(defvar my-term-shell "/bin/zsh")
+(defadvice ansi-term (before force-bash)
+  (interactive (list my-term-shell)))
+(ad-activate 'ansi-term)
+
 (use-package which-key
   :ensure t
   :init (which-key-mode))
@@ -10,10 +15,25 @@
   :ensure t
   :config (add-hook 'org-mode-hook (lambda () (org-bullets-mode))))
 
-(defvar my-term-shell "/bin/zsh")
-(defadvice ansi-term (before force-bash)
-  (interactive (list my-term-shell)))
-(ad-activate 'ansi-term)
+(setq ido-enable-flex-matching nil)
+(setq ido-create-new-buffer 'always)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+(use-package ido-vertical-mode
+  :ensure t
+  :init (ido-vertical-mode 1))
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
+
+(use-package smex
+  :ensure t
+  :init (smex-initialize))
+
+(global-set-key (kbd "C-x C-b") 'ido-switch-buffer)
+
+(global-set-key (kbd "C-x b") 'ibuffer)
+
+(setq ibuffer-expert t)
 
 (setq inhibit-startup-message t)
 (tool-bar-mode -1)
@@ -28,6 +48,7 @@
 (when window-system (global-prettify-symbols-mode t))
 (setq make-backup-file nil)
 (setq auto-save-default nil)
+(global-set-key (kbd "M-x") 'smex)
 
 (set-frame-position (selected-frame) 50 50)
 (setq initial-frame-alist
