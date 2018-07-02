7cd24dbe20cbf81e36c6b86cca978e39173793f9
Alex Pittendreigh <Alex.Pittendreigh@gmail.com>
2018-07-02 21:08:28 +0800

    Added in the neotree sidebar file navigator

--- init.el
+++ init.el
@@ -30,7 +30,9 @@
  '(org-agenda-files nil)
  '(package-selected-packages
    (quote
-    (phpcbf yasnippet-snippets ac-php flycheck web-mode php-mode ac-emmet emmet-mode auto-complete company-irony pretty-mode expand-region mark-multiple swiper popup-kill-ring symon dmenu diminish spaceline company rainbow-delimiters sudo-edit hungry-delete switch-window rainbow-mode avy smex ido-vertical-mode org-bullets beacon which-key use-package spacemacs-theme))))
+    (all-the-icons neotree phpcbf yasnippet-snippets ac-php flycheck web-mode php-mode ac-emmet emmet-mode auto-complete company-irony pretty-mode expand-region mark-multiple swiper popup-kill-ring symon dmenu diminish spaceline company rainbow-delimiters sudo-edit hungry-delete switch-window rainbow-mode avy smex ido-vertical-mode org-bullets beacon which-key use-package spacemacs-theme)))
+ '(phpcbf-executable "/Users/alex/.composer/vendor/bin/phpcbf")
+ '(phpcbf-standard "PSR12"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
